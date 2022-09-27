2e6e4a276a1076a366eb3f83291046e4448a074a
Andrew690 <113254963+Andrew690@users.noreply.github.com>
2022-09-27 16:27:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var video = document.getElementById('video');
 var player = new shaka.Player(video);
 window.player = player;
 
-var manifestUrl = 'https://etslive-2-vidio-com.akamaized.net/exp=1664281111~acl=/hls-b/ingest_9858_*/*~hmac=cb835256937afd5f9c258336309deea9158d676eb035b71a19fb159404ec338a/hls-b/ingest_9858_1080p/index.m3u8?&m=1';    
+var manifestUrl = 'https://etslive-2-vidio-com.akamaized.net/exp=1664281111~acl=/hls-b/ingest_9858_*/*~hmac=cb835256937afd5f9c258336309deea9158d676eb035b71a19fb159404ec338a/hls-b/ingest_9858_1080p/index.m3u8';    
 
 player.load(manifestUrl);
 
